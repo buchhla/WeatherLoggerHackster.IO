9dd45b850bae232976f3e9dce3b1f3caae9bd853
buchhla <buchhla@gmail.com>
2016-01-20 22:36:09 -0800

    V2, added I2C LCD and TCS34725 light sensor

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 using Windows.Devices.Gpio;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Media;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using Windows.Media.SpeechSynthesis;
+using Windows.UI.Core;
 
 
 namespace WeatherLogger
@@ -19,27 +17,71 @@ namespace WeatherLogger
         private const int LED_PIN = 12;
         private GpioPin pin;
         private GpioPinValue pinValue;
-        private DispatcherTimer timer;
-        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
-        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
-
+        private DispatcherTimer LoggerTimer;
+        private DispatcherTimer Timer;
+        //A class which wraps the color sensor
+        TCS34725 colorSensor;
+        //A SpeechSynthesizer class for text to speech operations
+        SpeechSynthesizer synthesizer;
+        //A MediaElement class for playing the audio
+        MediaElement audio;
         //A class which wraps the barometric sensor
         BMP280 BMP280;
+        //A GPIO pin for the pushbutton
+        GpioPin buttonPin;
+        //The GPIO pin number we want to use to control the pushbutton
+        int gpioPin = 4;
 
+        private const string I2C_CONTROLLER_NAME = "I2C1"; //use for RPI2
+        private const byte LCD_I2C_ADDRESS = 0x3F; // 7-bit I2C address of the port expander
+
+        //Setup pins
+        private const byte EN = 0x02;
+        private const byte RW = 0x01;
+        private const byte RS = 0x00;
+        private const byte D4 = 0x04;
+        private const byte D5 = 0x05;
+        private const byte D6 = 0x06;
+        private const byte D7 = 0x07;
+        private const byte BL = 0x03;
+        displayI2C lcd = new displayI2C(LCD_I2C_ADDRESS, I2C_CONTROLLER_NAME, RS, RW, EN, D4, D5, D6, D7, BL);
 
         public MainPage()
         {
             InitializeComponent();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMinutes(1);
-            timer.Tick += Timer_Tick;
+            lcd.init();
+
+            LoggerTimer = new DispatcherTimer();
+            LoggerTimer.Interval = TimeSpan.FromMinutes(1);
+            LoggerTimer.Tick += LoggerTimer_Tick;
+
+            Timer = new DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(1);
+            Timer.Tick += Timer_Tick;
+
             InitGPIO();
+
+            //Create a new SpeechSynthesizer
+            synthesizer = new SpeechSynthesizer();
+
+            //Create a new MediaElement
+            audio = new MediaElement();
+            
+
+            lcd.turnOnBacklight();
+            lcd.clrscr();
+            lcd.gotoxy(0, 0);
+            lcd.prints("SV Nokomis Pi Logger");
+            //StopwatchDelay.Delay(250);
+
             if (pin != null)
             {
-                Timer_Tick(this,null);
-                timer.Start();
-            }        
+                LoggerTimer_Tick(this, null);
+                LoggerTimer.Start();
+                //Timer_Tick(this, null);
+                //Timer.Start();
+            }
         }
 
         private void InitGPIO()
@@ -50,7 +92,7 @@ namespace WeatherLogger
             if (gpio == null)
             {
                 pin = null;
-                GpioStatus.Text = "There is no GPIO controller on this device.";
+                //GpioStatus.Text = "There is no GPIO controller on this device.";
                 return;
             }
 
@@ -59,11 +101,11 @@ namespace WeatherLogger
             pin.Write(pinValue);
             pin.SetDriveMode(GpioPinDriveMode.Output);
 
-            GpioStatus.Text = "GPIO pin initialized correctly.";
+            //GpioStatus.Text = "GPIO pin initialized correctly.";
 
         }
 
-   private async Task<WeatherData> ReadWeatherData()
+        private async Task<WeatherData> ReadWeatherData()
         {
             WeatherData wd = null;
             try
@@ -105,10 +147,60 @@ namespace WeatherLogger
             return wd;
         }
 
+        private async Task<LightData> ReadLightData()
+        {
+            LightData ld = null;
+            try
+            {
+                if (colorSensor == null)
+                {
+                    colorSensor = new TCS34725();
+                    await colorSensor.Initialize();
+                }
 
+                //Read the approximate color from the sensor
+                string colorRead = await colorSensor.getClosestColor();
+                //Output the colr name to the speaker
+                await Speak(" The current color is: " + colorRead);
+                RgbData rgb = await colorSensor.getRgbData();
 
+                float lux = TCS34725.getLuxSimple(rgb);
+                ld = new LightData();
+                ld.Created = DateTime.Now;
+                ld.rgbData = rgb;
+                ld.Lux = lux;
+                ld.ColorTempinK = TCS34725.calculateColorTemperature(rgb);
+                Debug.WriteLine("Current lux: " + lux);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return ld;
+        }
+        //This method is used to output a string to the speaker
+        private async Task Speak(string textToSpeak)
+        {
+            //Create a SpeechSynthesisStream using a string
+            var stream = await synthesizer.SynthesizeTextToStreamAsync(textToSpeak);
+            //Use a dispatcher to play the audio
+            var ignored = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                //Set the souce of the MediaElement to the SpeechSynthesisStream
+                audio.SetSource(stream, stream.ContentType);
+                //Play the stream
+                audio.Play();
+            });
+        }
 
         private void Timer_Tick(object sender, object e)
+        {
+            lcd.gotoxy(10, 3);
+            lcd.prints("Time:" + DateTime.Now.ToString("hh:mm"));
+        }
+
+
+        private void LoggerTimer_Tick(object sender, object e)
         {
             if (pinValue == GpioPinValue.High)
             {
@@ -127,12 +219,27 @@ namespace WeatherLogger
                     txtTemp.Text = "Temperature: " + wd.TemperatureinF + " deg F";
                     Debug.WriteLine("Pressure: " + wd.Pressureinmb + " mb");
                     txtPressure.Text = "Pressure: " + wd.Pressureinmb + " mb";
-                   // string json = JsonConvert.SerializeObject(wd);
-
+                    // string json = JsonConvert.SerializeObject(wd);
+                    lcd.gotoxy(0, 1);
+                    lcd.prints("Temperature:" + Math.Round(wd.TemperatureinF,2));
+                    lcd.gotoxy(0, 2);
+                    lcd.prints("Pressure: " + Math.Round(wd.Pressureinmb,2));
                     Debug.WriteLine("");
 
                 }, TaskScheduler.FromCurrentSynchronizationContext());
+                ReadLightData().ContinueWith((t) =>
+                {
+                    LightData ld = t.Result;
 
+                    //AdafruitIO io = new AdafruitIO();
+                    //io.sendData(ld);
+                    Debug.WriteLine("Lux: " + ld.Lux);
+                    Debug.WriteLine("Color Temp:" + ld.ColorTempinK + " K");
+                    // string json = JsonConvert.SerializeObject(wd);
+                    lcd.gotoxy(0, 3);
+                    lcd.prints("Lux:" + Math.Round(ld.Lux,2));
+
+                }, TaskScheduler.FromCurrentSynchronizationContext());
                 pinValue = GpioPinValue.High;
                 pin.Write(pinValue);
             }
@@ -142,7 +249,21 @@ namespace WeatherLogger
                 pin.Write(pinValue);
             }
         }
-             
+
+        private void InitializeButtonGpio()
+        {
+            //Create a default GPIO controller
+            GpioController gpioController = GpioController.GetDefault();
+            //Use the controller to open the gpio pin of given number
+            buttonPin = gpioController.OpenPin(gpioPin);
+            //Debounce the pin to prevent unwanted button pressed events
+            buttonPin.DebounceTimeout = new TimeSpan(1000);
+            //Set the pin for input
+            buttonPin.SetDriveMode(GpioPinDriveMode.Input);
+            //Set a function callback in the event of a value change
+            buttonPin.ValueChanged += LoggerTimer_Tick;
+        }
 
     }
 }
+
